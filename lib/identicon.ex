701d931b798cfe6bf9159b611f3109cc6fe5f10c
Andrea Orler <andrea@codegram.com>
2020-07-04 15:14:52 +0200

    definr mirror_row function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -11,6 +11,10 @@ defmodule Identicon do
     |> Enum.chunk(3)
   end
 
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
+  end
+
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
