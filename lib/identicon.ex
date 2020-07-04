f83addb065c4599e4035339fad837b4bc7d1ef7d
Andrea Orler <andrea@codegram.com>
2020-07-04 14:30:04 +0200

    hash string

---
+++ lib/identicon.ex
@@ -0,0 +1,11 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_string
+  end
+
+  def hash_string(input) do
+    :crypto.hash :md5, input
+    |> :binary.bin_to_list
+  end
+end
